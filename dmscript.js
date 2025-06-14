0c31acb27c53f329df841a171752e469cf61cb2e
LunkyWonky <luzhu9572@gmail.com>
2025-06-14 19:05:51 -0400

    hopefully erroe fixed

--- dmscript.js
+++ dmscript.js
@@ -1,5 +1,6 @@
 const button = document.getElementById('themetoggle');
     button.addEventListener('click', () => {
       document.body.classList.toggle('dark');
+      document.p.classList.toggle('dark');
       button.textContent = document.body.classList.contains('dark') ? "☀️" : "🌙";
     });
